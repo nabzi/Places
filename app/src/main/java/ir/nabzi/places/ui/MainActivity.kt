87317fa6e05bc86cd3280c80ea98420bd1cf1e01
zmodir <modir@techranco.com>
2021-04-25 14:14:03 +0430

    Added some dependencies

--- app/src/main/java/ir/nabzi/places/ui/MainActivity.kt
+++ app/src/main/java/ir/nabzi/places/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package ir.nabzi.places
+package ir.nabzi.places.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ir.nabzi.places.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
