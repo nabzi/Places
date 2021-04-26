bf36be4d278f844969924f0448378c17bc841c0a
z.modir <zeinab.modir@gmail.com>
2021-04-26 19:42:18 +0430

    Navigate to placeDetail

--- app/src/main/java/ir/nabzi/places/ui/home/PlacesFragment.kt
+++ app/src/main/java/ir/nabzi/places/ui/home/PlacesFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.annotations.MarkerOptions
@@ -41,7 +42,11 @@ class PlacesFragment : Fragment() {
             override fun createFragment(position: Int): Fragment {
                 return PlaceItemFragment.create(
                     places[position]
-                ) { id -> Toast.makeText(requireContext(), id, Toast.LENGTH_SHORT).show() }
+                ) { id -> Toast.makeText(requireContext(), id, Toast.LENGTH_SHORT).show()
+                    findNavController().navigate(
+                        PlacesFragmentDirections.actionPlacesFragmentToPlaceDetailsFragment(places[position].id)
+                    )
+                }
             }
 
             override fun getItemCount(): Int {
